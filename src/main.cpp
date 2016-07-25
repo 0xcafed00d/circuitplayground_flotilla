e5ee08c06188a1a134497aeaa89ca6d583db2e15
Lee Witek <neomech@gmail.com>
2016-07-25 22:20:47 +0100

    added custom board definition for circuit playground & added cap touch module

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,37 @@ class CPGModuleButtons : public ModuleTouch {
 	}
 };
 
+class CPGModuleTouch : public ModuleTouch {
+  public:
+	CPGModuleTouch(int b1, int b2, int b3, int b4) : m_b1(b1), m_b2(b2), m_b3(b3), m_b4(b4) {
+	}
+
+  protected:
+	virtual uint8_t GetState() {
+		uint8_t state = 0;
+
+		if (CircuitPlayground.readCap(m_b1) > 100)
+			state |= 1;
+
+		if (CircuitPlayground.readCap(m_b2) > 100)
+			state |= 2;
+
+		if (CircuitPlayground.readCap(m_b3) > 100)
+			state |= 4;
+
+		if (CircuitPlayground.readCap(m_b4) > 100)
+			state |= 8;
+
+		return state;
+	}
+
+  private:
+	int m_b1;
+	int m_b2;
+	int m_b3;
+	int m_b4;
+};
+
 class CPGModuleLight : public ModuleLight {
   protected:
 	virtual void GetState(uint16_t& visible, uint16_t& ir, uint16_t& lux) {
@@ -61,6 +92,7 @@ CPGModuleRainbow rainbow2(5);
 CPGModuleButtons touch1;
 CPGModuleLight light;
 CPGModuleMotion motion;
+CPGModuleTouch touch2(12, 6, 9, 10);
 
 void setup() {
 	CircuitPlayground.begin();
@@ -73,18 +105,18 @@ void setup() {
 	touch1.Init(3);
 	light.Init(4);
 	motion.Init(5);
+	touch2.Init(6);
 
 	dock.AddModule(&rainbow1);
 	dock.AddModule(&rainbow2);
 	dock.AddModule(&touch1);
 	dock.AddModule(&light);
 	dock.AddModule(&motion);
+	dock.AddModule(&touch2);
 }
 
 void loop() {
-	Serial.println(CircuitPlayground.readCap(1));
-
-	// dock.Update(&Serial);
-	// dock.ProcessInput(&Serial);
+	dock.Update(&Serial);
+	dock.ProcessInput(&Serial);
 	delay(100);
 }
